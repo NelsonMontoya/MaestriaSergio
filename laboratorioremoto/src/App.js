88e20964486fdf6358f038484146b2ca83c0b338
NelsonMontoya <andresito.asb@gmail.com>
2022-06-10 18:36:13 -0500

    Inicial

---
+++ laboratorioremoto/src/App.js
@@ -0,0 +1,11 @@
+import Tarea from "./components/tareas/Tarea";
+
+
+
+function App() {
+  return (
+    <Tarea />
+  );
+}
+
+export default App;
---
+++ laboratorioremoto/src/components/tareas/Tarea.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Tarea = () => {
+  return (
+    <>
+      <h1>Hola Lindo</h1>
+    </>
+  )
+}
+
+export default Tarea
+
